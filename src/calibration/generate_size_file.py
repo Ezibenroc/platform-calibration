0f9441f8a6e8df814ba2514d7262de771a81ae3d
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2018-08-02 15:05:55 +0200

    Fix size generation

--- src/calibration/generate_size_file.py
+++ src/calibration/generate_size_file.py
@@ -2,7 +2,7 @@ import itertools
 import random
 
 op = ['Recv', 'Isend', 'PingPong', 'Wtime', 'Wtime', 'Iprobe', 'Test']
-sizes = [10**random.uniform(0, 9) for _ in range(100)]
+sizes = [int(10**random.uniform(0, 9)) for _ in range(100)]
 exp = list(itertools.product(op, sizes))
 exp *= 50
 random.shuffle(exp)
